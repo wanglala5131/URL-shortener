3ee0ba6de3565badd3a9585c6ee458cd21c17664
Wanglala <sue4100035045@gmail.com>
2020-05-26 22:43:47 +0800

    refactor: routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const Url = require('./models/url')
 const randomNumber = require('./randomNumber.js')
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
@@ -12,55 +13,58 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-app.post('/', (req, res) => {
-  const url = req.body.url
-  if (url.includes('http://') || url.includes('https://')) {
-    if (url.includes('http://localhost:3000/') || url.includes('https://localhost:3000/')) {
-      const message = '請勿輸入此網頁或已被縮短的網頁'
-      res.render('index', { message, url })
-    } else {
-      Url.find()
-        .lean()
-        .then(existUrl => {
-          for (let i = 0; i < existUrl.length; i++) {  //check資料庫中是否有資料
-            if (existUrl[i].originalUrl === url) {
-              return res.render('result', { shortUrl: existUrl[i].shortUrl })
-            }
-          }
-          let shortUrl = randomNumber()
-          Url.create({
-            originalUrl: url,
-            shortUrl: shortUrl
-          })
-          return res.render('result', { shortUrl })
-        })
-        .catch(err => console.log(err))
-    }
+// const INDEX_URL = `http://localhost:${PORT}/`
 
-  } else {
-    const message = '請輸入有效網址'
-    res.render('index', { message, url })
-  }
-})
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
+// app.post('/', (req, res) => {
+//   const url = req.body.url
+//   if (url.includes('http://') || url.includes('https://')) {
+//     if (url.includes(INDEX_URL)) {
+//       const message = '請勿輸入此網頁或已被縮短的網頁'
+//       res.render('index', { message, url })
+//     } else {
+//       Url.find()
+//         .lean()
+//         .then(existUrl => {
+//           for (let i = 0; i < existUrl.length; i++) {  //check資料庫中是否有資料
+//             if (existUrl[i].originalUrl === url) {
+//               return res.render('result', { shortUrl: existUrl[i].shortUrl, INDEX_URL })
+//             }
+//           }
+//           let shortUrl = randomNumber()
+//           Url.create({
+//             originalUrl: url,
+//             shortUrl: shortUrl
+//           })
+//           return res.render('result', { shortUrl, INDEX_URL })
+//         })
+//         .catch(err => console.log(err))
+//     }
 
-app.get('/:currentUrl', (req, res) => {
-  const { currentUrl } = req.params
-  Url.find()
-    .lean()
-    .then(existUrl => {
-      for (let i = 0; i < existUrl.length; i++) {  //check資料庫中是否有資料
-        if (existUrl[i].shortUrl === currentUrl) {
-          return res.redirect(existUrl[i].originalUrl)
-        }
-      }
-      return res.render('error')
-    })
-    .catch(err => console.log(err))
-})
+//   } else {
+//     const message = '請輸入有效網址'
+//     res.render('index', { message, url })
+//   }
+// })
+
+// app.get('/:currentUrl', (req, res) => {
+//   const { currentUrl } = req.params
+//   Url.find()
+//     .lean()
+//     .then(existUrl => {
+//       for (let i = 0; i < existUrl.length; i++) {  //check資料庫中是否有資料
+//         if (existUrl[i].shortUrl === currentUrl) {
+//           return res.redirect(existUrl[i].originalUrl)
+//         }
+//       }
+//       return res.render('error')
+//     })
+//     .catch(err => console.log(err))
+// })
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`)
---
+++ routes/modules/home.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const router = express.Router()
+
+const Url = require('../../models/url')
+const randomNumber = require('../../randomNumber')
+const PORT = 3000
+const INDEX_URL = `http://localhost:${PORT}/`
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+router.post('/', (req, res) => {
+  const url = req.body.url
+  if (url.includes('http://') || url.includes('https://')) {
+    if (url.includes(INDEX_URL)) {
+      const message = '請勿輸入此網頁或已被縮短的網頁'
+      res.render('index', { message, url })
+    } else {
+      Url.find()
+        .lean()
+        .then(existUrl => {
+          for (let i = 0; i < existUrl.length; i++) {  //check資料庫中是否有資料
+            if (existUrl[i].originalUrl === url) {
+              return res.render('result', { shortUrl: existUrl[i].shortUrl, INDEX_URL })
+            }
+          }
+          let shortUrl = randomNumber()
+          Url.create({
+            originalUrl: url,
+            shortUrl: shortUrl
+          })
+          return res.render('result', { shortUrl, INDEX_URL })
+        })
+        .catch(err => console.log(err))
+    }
+
+  } else {
+    const message = '請輸入有效網址'
+    res.render('index', { message, url })
+  }
+})
+
+router.get('/:currentUrl', (req, res) => {
+  const { currentUrl } = req.params
+  Url.find()
+    .lean()
+    .then(existUrl => {
+      for (let i = 0; i < existUrl.length; i++) {  //check資料庫中是否有資料
+        if (existUrl[i].shortUrl === currentUrl) {
+          return res.redirect(existUrl[i].originalUrl)
+        }
+      }
+      return res.render('error')
+    })
+    .catch(err => console.log(err))
+})
+
+module.exports = router

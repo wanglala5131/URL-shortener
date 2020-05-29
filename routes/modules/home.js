0c3436b2bac69e7bca3c741a0e85b505a8793be7
Wanglala <sue4100035045@gmail.com>
2020-05-29 18:39:37 +0800

    refactor: modify home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,7 +5,7 @@ const Url = require('../../models/url')
 const randomNumber = require('../../randomNumber')
 const PORT = process.env.PORT || 3000
 const INDEX_URL = 'https://hidden-caverns-24251.herokuapp.com'  //記得不要加最後面的/
-// const INDEX_URL =`http://localhost:${PORT}`
+//const INDEX_URL = `http://localhost:${PORT}`
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -17,15 +17,19 @@ router.post('/', (req, res) => {
       const message = '請勿輸入此網頁或已被縮短的網頁'
       res.render('index', { message, url })
     } else {
-      Url.find()
+      Url.findOne({ originalUrl: url })    //Url.find()  
         .lean()
         .then(existUrl => {
-          for (let i = 0; i < existUrl.length; i++) {  //check資料庫中是否有資料
-            if (existUrl[i].originalUrl === url) {
-              return res.render('result', { shortUrl: existUrl[i].shortUrl, INDEX_URL })
-            }
+          //助教建議，用迴圈的話，若資料太多效能會變差，建議修改
+          // for (let i = 0; i < existUrl.length; i++) {  //check資料庫中是否有資料
+          //   if (existUrl[i].originalUrl === url) {
+          //     return res.render('result', { shortUrl: existUrl[i].shortUrl, INDEX_URL })
+          //   }
+          // }
+          if (existUrl) {   //如果有資料庫有符合就return
+            return res.render('result', { shortUrl: existUrl.shortUrl, INDEX_URL })
           }
-          let shortUrl = randomNumber()
+          let shortUrl = randomNumber()   //資料庫沒有符合就新增資料庫
           Url.create({
             originalUrl: url,
             shortUrl: shortUrl
